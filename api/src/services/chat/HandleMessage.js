865fa4f6525f3480ec9a256ad9cf0baca07b103a
Victor Hugo <vhuugoc@gmail.com>
2020-08-27 13:52:11 -0300

    Criação do chat e gravação da mensagem ao enviar

--- api/src/services/chat/HandleMessage.js
+++ api/src/services/chat/HandleMessage.js
@@ -1,9 +1,31 @@
 /** @module HandleMessage */
 
+const model = require('../../configs/database/connection');
 const configs = require('../../configs/credentials/whatsapp.json');
+const moment = require('moment');
+
+const HandleChat = require('./HandleChat');
 var req = require('request');
 
 class HandleMessage {
+  /**
+   * Add a message
+   * @param {string} contact_id 
+   * @param {string} text 
+   * @param {string} type 
+   * @param {string} url 
+   * @param {string} from 
+   * @param {boolean} has_seen 
+   */
+  async add(contact_id, text, type, url = '', from, has_seen = false) {
+    let chat = await HandleChat.get(contact_id);
+
+    const timestamp = moment().utc().format();
+    chat.messages.push({ text, type, url, from, has_seen, timestamp });
+
+    await model.collection('chat').doc(chat.id).set(chat);
+  }
+
   /**
    * Sends a message
    * @param {string} message 
@@ -12,7 +34,7 @@ class HandleMessage {
    * @param {string} channel
    * @return boolean
    */
-  send(message, type, destination, channel = "whatsapp") {
+  async send(message, type, destination, channel = "whatsapp") {
     var options = {
       'method': 'POST',
       'url': `${configs.url}/msg`,
@@ -33,6 +55,10 @@ class HandleMessage {
       if (error) throw new Error(error);
       console.log(response.body);
     });
+
+    await HandleChat.add(destination);
+
+    await this.add('5513996096926', message, type, '', 'me');
 
   }
 }
